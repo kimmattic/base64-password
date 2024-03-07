4a39035c3d548cd4efa6474af0d28eff9d92623a
Sorlavit Nimmarlairatana <sorlavit.nim@gmail.com>
2024-03-07 12:17:24 +0700

    Create decode.py

---
+++ decode.py
@@ -0,0 +1,11 @@
+#Lets create a password encryption program
+
+import base64
+
+def decode_pass(password):
+    decode_bytes = base64.b64decode(password)
+    decode_data = decode_bytes.decode()
+    print(f"Decoded password {decode_data}")
+
+encode_string = input("enter the base64 string: ")
+decode_pass(encode_string)
